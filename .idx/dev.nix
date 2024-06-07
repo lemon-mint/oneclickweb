bc3e5e8e82227dc6256be07a484efd4ff7f71c11
ice.lemons.mint <ice.lemons.mint@gmail.com>
2024-06-07 08:01:18 +0000

    Update Nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,15 +2,16 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
+  channel = "unstable"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
-    pkgs.nodejs_20
+    pkgs.nodejs_22
     # pkgs.nodePackages.nodemon
+    pkgs.bun
   ];
 
   # Sets environment variables in the workspace
@@ -19,6 +20,8 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "golang.go"
+      "oven.bun-vscode"
     ];
 
     # Enable previews
